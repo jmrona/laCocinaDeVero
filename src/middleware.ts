e13394f3dd2fe02a6f3d379a6bf1b896ec1107f4
Jose Romero <jose@forthwithlife.co.uk>
2025-04-13 21:41:45 +0100

    Add analytics

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,12 @@ import { defineMiddleware } from "astro:middleware";
 
 
 export const onRequest = defineMiddleware((context, next) => {
-    // Middleware para redirigir a /es si la URL es la raíz
     const { request } = context;
     const { pathname } = new URL(request.url);
     
-    // Si está en la raíz, redirigir a /es
     if (pathname === '/') {
         return context.redirect('/es');
     }
     
-    return next(); // continuar con la petición normal
+    return next();
 });
