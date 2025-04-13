00b4293a61bbab6182a00895cc1489ddea72cc5a
Jose Romero <jose@forthwithlife.co.uk>
2025-04-13 21:29:47 +0100

    Add Vercel dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import vercel from "@astrojs/vercel";
+
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   i18n: {
     locales: ["es", "en", "de"],
     defaultLocale: "es"
   },
+
+  adapter: vercel(),
 });
