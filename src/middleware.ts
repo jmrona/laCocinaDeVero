fb7088f5ca78328afd9149aa36064910aa83d257
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-14 17:56:31 +0100

    get default language from the browser

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,11 @@ export const onRequest = defineMiddleware(async (context, next) => {
     const {pathname} = new URL(context.request.url);
     
     if (pathname === '/') {
-        return Response.redirect(new URL('/es', context.request.url), 307);
+        const lang = context.request.headers.get("accept-language")?.split(",")[0].split("-")[0] || "es";
+        const supported = ["es", "en", "de"];
+        const redirectLang = supported.includes(lang) ? lang : "es";
+
+        return Response.redirect(new URL(`/${redirectLang}`, context.request.url), 307);
     }
     
     return next();
