7621e371672a2147f130c2600d580c9880162eb3
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-14 17:49:22 +0100

    debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { defineMiddleware } from "astro:middleware";
 
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    const {pathname} = new URL(context.url);
+    const {pathname} = new URL(context.request.url);
     
     if (pathname === '/') {
-        return context.redirect('/es');
+        return Response.redirect(new URL('/es', context.request.url), 307);
     }
     
     return next();
