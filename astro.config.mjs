a748d2391221cf5c79459daca85a46fd6a133522
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-18 19:54:13 +0100

    Add React and sitemap integration, update configuration for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
-
 import vercel from "@astrojs/vercel";
-
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   vite: {
@@ -15,6 +14,17 @@ export default defineConfig({
     locales: ["es", "en", "de"],
     defaultLocale: "es"
   },
+  site: "https://lacocinadevero.es",
   output: "server",
   adapter: vercel(),
+  integrations: [react(), sitemap({
+    i18n: {
+        defaultLocale: 'es',
+        locales: {
+          en: 'en',
+          es: 'es',
+          de: 'de',
+        },
+      },
+  })],
 });
