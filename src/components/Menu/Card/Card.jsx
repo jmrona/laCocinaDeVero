ed3183e9d06a6e4e72fe539916e29efc6ecf0d98
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-21 15:23:47 +0100

    Fix layout

--- src/components/Menu/Card/Card.jsx
+++ src/components/Menu/Card/Card.jsx
@@ -15,14 +15,14 @@ export default function Card({food, className = "", lang = "es", useHorizontal =
             className={`aspect-[1/0.8] object-cover rounded-lg mb-2 ${useHorizontal ? "w-[40%] h-full md:h-auto object-cover md:w-full" : "w-full"}`} /> 
         : null}
 
-      <div className={`flex flex-col gap-2 p-4 ${hasImage ? "mb-3" : ""}`}>
+      <div className={`flex flex-col gap-2 p-4 h-full ${hasImage ? "mb-3" : ""}`}>
         <h2 className='text-lg font-semibold text-balance leading-5'>{food.name}</h2>
         <div className='flex flex-row gap-2 items-center'>
             <span className='text-md text-primary font-semibold'>{food.price}€</span>  
             {food.calories != 0 && <span className='text-sm text-gray-700'>|</span>} 
             {food.calories != 0 && <span className='text-sm text-gray-700'>{food.calories} cal</span> } 
         </div>
-        <p className='leading-5 text-sm text-gray-700 text-pretty'>{food.description}</p>
+        <p className='grow leading-5 text-sm text-gray-600 text-pretty'>{food.ingredients.join(", ")}</p>
 
         {food.allergens.length > 0 && (
           <>
