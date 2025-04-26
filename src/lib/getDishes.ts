e80961f7e61b3ade8ae1c5a6a2505cd5cf316565
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-26 21:43:15 +0100

    Read dishes from supabase

--- src/lib/getDishes.ts
+++ src/lib/getDishes.ts
@@ -1,11 +1,42 @@
+import { supabase } from "@/db/supabase";
+
 type LangType = 'es' | 'en' | 'de';
 
 export const getDishes = async (lang: LangType) => {
-  const res = await import('../db/dishes.json')
-
-  if (!res) {
-      throw new Error('Error loading categories')
+  const { data, error } = await supabase
+  .from('dishes')
+  .select(`
+    dish_id,
+    name:name->>${lang},
+    price,
+    image,
+    dishes_categories!inner(
+      categories!inner(
+        name, category_id
+      )
+    ),
+    categories:dishes_categories( category_id, categories( name:name->>${lang} )),
+    allergens:dishes_allergens( allergen_id )
+  `);
+  
+  if (error) {
+    console.error('Error fetching dishes:', error.message);
+    return [];
   }
+  
+  return data.map(dish => {
+      const categories = dish.categories.map(cat => cat.category_id)
+      const allergens = dish.allergens.map(allergen => allergen.allergen_id)
+
+      return {
+          id: dish.dish_id,
+          name: dish.name,
+          price: dish.price,
+          image: dish.image,
+          categories,
+          allergens
+      }
+  });
 
 
   const dishes = res.default.map((dish) => {
