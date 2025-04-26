23631a35baf269cfea3d31e809007ea457d91eb3
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-26 21:37:30 +0100

    Implement supabase

--- src/lib/getAllergens.ts
+++ src/lib/getAllergens.ts
@@ -1,22 +1,15 @@
+import { supabase } from "@/db/supabase";
+
 type LangType = 'es' | 'en' | 'de';
 
 export const getAllergens = async (lang: LangType) => {
-  const res = await import('../db/allergens.json')
+  const { data, error } = await supabase
+  .from('allergens')
+  .select(`id:allergen_id, name:name->>${lang}, icon`)
 
-  if (!res) {
-      throw new Error('Error loading categories')
-  }
-
-
-  const allergens = res.default.map((allergen) => {
-    return ({
-      id: allergen.id,
-      name: allergen.name[lang] || allergen.name['es'] || allergen.name['en'] || allergen.name['de'],
-      icon: allergen.icon
-    })
-  })
-
-  return allergens;
+  if (error) return []
+  
+  return data
 }
 
 export interface AllergensType {
