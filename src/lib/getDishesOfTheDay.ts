e6aaffbb89641b91b9b77ac1c594e79d9002aba9
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-26 21:30:24 +0100

    Add supabase

--- src/lib/getDishesOfTheDay.ts
+++ src/lib/getDishesOfTheDay.ts
@@ -1,16 +1,4 @@
-import categories from "@/db/categories.json"
-import dishes from "@/db/dishes.json"
-console.log('dishes: ', dishes);
-
-const DAY_CATEGORY = {
-    0: ["Domingo", "Sunday", "Sonntag"],
-    1: ["Lunes", "Monday", "Montag"],
-    2: ["Martes", "Tuesday", "Dienstag"],
-    3: ["Miércoles", "Wednesday", "Mittwoch"],
-    4: ["Jueves", "Thursday", "Donnerstag"],
-    5: ["Viernes", "Friday", "Freitag"],
-    6: ["Sábado", "Saturday", "Samstag"],
-} as const;
+import { supabase } from "@/db/supabase";
 
 const WEEK_DAYS = {
     "es": ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"], 
@@ -21,19 +9,52 @@ const WEEK_DAYS = {
 const today = new Date()
 const dayOfWeek = today.getDay()
 
-
 export const getDishesOfTheDay = async (lang: "es" | "en" | "de") => { 
-    const day = WEEK_DAYS[lang][dayOfWeek]
+    const today = WEEK_DAYS[lang][dayOfWeek]
+
+    const { data: category, error: categoriesError } = await supabase
+        .from('categories')
+        .select('category_id')
+        .in(`name->>${lang}`, [today]);
     
-    const categoryId = Object.values(DAY_CATEGORY).findIndex(category => category.some(cat => cat === day))
-    
-    if( categoryId === -1) return []
-    const category = categories.find(category => category.id === categoryId)
-    
-    return dishes.filter(dish => dish.category.some(cat => cat === category?.id)).map(dish => ({
-        ...dish,
-        name: dish.name[lang],
-        ingredients: dish.ingredients[lang],
-        description: dish.description[lang]
-    }))
+    if(categoriesError) return []
+    const categoryId: number | undefined = category?.[0]?.category_id
+
+      
+    const { data, error } = await supabase
+      .from('dishes')
+      .select(`
+        dish_id,
+        name:name->>${lang},
+        price,
+        image,
+        dishes_categories!inner(
+          categories!inner(
+            name, category_id
+          )
+        ),
+        categories:dishes_categories( category_id, categories( name:name->>${lang} )),
+        allergens:dishes_allergens( allergen_id )
+      `)
+      .in('dishes_categories.category_id', [categoryId]);
+
+    if (error) {
+      console.error('Error fetching dishes:', error.message);
+      return [];
+    }
+
+    return data.map(dish => {
+        const categories = dish.categories.map(cat => cat.category_id)
+        const allergens = dish.allergens.map(allergen => allergen.allergen_id)
+
+        return {
+            id: dish.dish_id,
+            name: dish.name,
+            price: dish.price,
+            image: dish.image,
+            categories,
+            allergens
+        }
+    });
+
 }
--- src/lib/getWeekMenu.ts
+++ src/lib/getWeekMenu.ts
@@ -1,20 +1,44 @@
-import categories from "@/db/categories.json"
-import dishes from "@/db/dishes.json"
-import type { DishesType } from "./getDishes"
+import { supabase } from "@/db/supabase"
 
-export const getWeekMenu = (lang: "es" | "en" | "de") => {
-    const menuGroupedByCategory = {} as Record<string, DishesType[]>
-    categories.slice(0,7).forEach((category) => {
-        const foods = dishes.filter(dish => dish.category.some(cat => cat === Number(category.id)))
-        const key = category.name[lang];
 
-        return menuGroupedByCategory[key] = foods.map(food => ({
-            ...food,
-            name: food.name[lang],
-            ingredients: food.ingredients[lang],
-            description: food.description[lang]
-        }))
-    })
+export const getWeekMenu = async (lang: "es" | "en" | "de") => { 
+    const { data, error } = await supabase
+      .from('dishes')
+      .select(`
+        dish_id,
+        name:name->>${lang},
+        price,
+        image,
+        dishes_categories!inner(
+          categories!inner(
+            name, category_id
+          )
+        ),
+        categories:dishes_categories( category_id, categories( name:name->>${lang} )),
+        allergens:dishes_allergens( allergen_id )
+      `)
+      .in('dishes_categories.categories.name->>es', ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]);
 
-    return menuGroupedByCategory
+    if (error) {
+      console.error('Error fetching dishes:', error.message);
+      return [];
+    }
+
+      
+    const groupedByDay = data.reduce<Record<string, string[]>>((acc, item) => {
+        const categoriesName: string[] = item.categories.map(cat => cat.categories.name)
+
+        categoriesName.forEach(category => {
+           
+            if(Object.keys(acc).includes(category)){
+                acc[category].push(item.name)
+            } else {
+                acc[category] = [item.name]
+            }
+        })
+
+        return acc;
+    }, {})
+
+    return groupedByDay;
 }
