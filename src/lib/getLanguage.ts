9a047a822700230dc582a692a005ad8645e38ff9
Jose Romero Navarro <jose@forthwithlife.co.uk>
2025-04-26 21:37:40 +0100

    Remove unused function

--- src/lib/getLanguage.ts
+++
@@ -1,9 +0,0 @@
-
-
-export function changeLanguage(lang: string) {
-  localStorage.setItem('language', lang);
-  const url = new URL(window.location.href);
-  const [language, ...rest] = url.pathname.split("/");
-  console.log(language)
-  // window.location.href = url.origin + url.pathname + "?lang=" + lang;
-}
